9c55a285d9259f79d589ca9f773865412531ca5b
siykt <cnsiykt@163.com>
2024-06-25 17:21:39 +0800

    feat: add auth jwt middleware

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -12,4 +12,8 @@ export const ENV = {
   APP_HOST: appHost,
   APP_PORT: +(process.env.APP_PORT ?? 10001),
   FILE_UPLOAD_SIZE: +(process.env.FILE_UPLOAD_SIZE ?? 10000000),
+
+  // Auth
+  SERVER_AUTH_PASSWORD: process.env.SERVER_AUTH_PASSWORD ?? 'esm-server-template',
+  JWT_SECRET: process.env.JWT_SECRET ?? 'esm-server-template',
 };
--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,6 +1,8 @@
 import { Context, Next } from 'koa';
+import jsonwebtoken from 'jsonwebtoken';
+import { ENV } from '../constants/env.js';
 
-export const psdAuth = (psd: string) => async (ctx: Context, next: Next) => {
+export const authPassword = (psd: string) => async (ctx: Context, next: Next) => {
   if (ctx.header.authorization !== psd) {
     ctx.status = 401;
     ctx.body = { code: 401, message: 'Unauthorized' };
@@ -9,3 +11,29 @@ export const psdAuth = (psd: string) => async (ctx: Context, next: Next) => {
     await next();
   }
 };
+
+type UserLick = {
+  id: string | number;
+};
+
+export const authJwt = (verify: (user: UserLick) => boolean | PromiseLike<boolean>, secret = ENV.JWT_SECRET) => {
+  return async (ctx: Context, next: Next) => {
+    try {
+      if (!ctx.header.authorization) throw new Error('No authorization header');
+      const user: UserLick = jsonwebtoken.verify(ctx.header.authorization, secret) as UserLick;
+
+      if (!verify(user)) throw new Error('Unauthorized');
+
+      ctx.state.user = user;
+    } catch (error) {
+      ctx.status = 401;
+      ctx.body = { code: 401, message: 'Unauthorized' };
+      return;
+    }
+
+    await next();
+  };
+};
+
+// self authJwt example
+// export const authSelfJwt = authJwt(async (user) => Boolean(await redis.get(user.id)));
